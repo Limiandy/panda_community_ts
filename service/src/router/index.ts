c97555d4b54d20f49b01452019a060cccd12a0a3
Andy <limengkai_a@163.com>
2021-05-12 08:54:48 +0800

    init koa config

--- service/src/router/index.ts
+++ service/src/router/index.ts
@@ -3,7 +3,7 @@ import requireContext from '@/utils/require-context'
 import path from 'path'
 
 const modPath = path.resolve(__dirname, 'modules')
-const moduleFiles = requireContext(modPath,  /\.ts$/)
+const moduleFiles = requireContext(modPath, /\.ts$/)
 const modules = moduleFiles.keys().reduce((items, path) => {
   const value = moduleFiles(path)
   items.push(value.default as never)
--- service/www/app.ts
+++ service/www/app.ts
@@ -1,14 +1,36 @@
 import moduleAlias from 'module-alias'
 import path from 'path'
 import Koa from 'koa'
-
+import KoaBody from 'koa-body'
+import cors from '@koa/cors'
+import KoaJson from 'koa-json'
+import statics from 'koa-static'
+import helmet from 'koa-helmet'
 import compose from 'koa-compose'
-import router from '../src/router'
+
 moduleAlias(path.resolve(__dirname, '..'))
+/* eslint-disable import/first */
+import router from '../src/router'
 
 const app = new Koa()
 
-const middleware = compose([router()])
+const middleware = compose([
+  KoaBody({
+    multipart: true,
+    formidable: {
+      keepExtensions: true,
+      maxFieldsSize: 5 * 1024 * 1024
+    },
+    onError: err => {
+      console.error(err)
+    }
+  }),
+  KoaJson(),
+  cors(),
+  helmet(),
+  statics(path.join(__dirname, '..', 'public')),
+  router()
+])
 
 app.use(middleware)
 
@@ -16,5 +38,5 @@ const BASE_URL: string = 'http://localhost'
 const PORT: number = 3000
 app.listen(PORT, () => {
   console.log('服务启动成功，正在监听' + PORT + '端口')
-  console.log('请访问：', BASE_URL, ':', PORT)
+  console.log('请访问：', BASE_URL + ':' + PORT + '/YouRestFulAPIs')
 })
